a867246803c098f2b87ab787dfcdfea25544b915
Jason Jackson <jasonc.jackson@gmail.com>
2024-11-04 15:44:53 -0500

    upgrade aws to 5.x

--- versions.tf
+++ versions.tf
@@ -3,12 +3,12 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
-      version = "~> 4.0"
       source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
     random = {
-      version = "~> 3.0"
       source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
