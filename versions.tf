bbbca28f866dc9ad3aa389a489c8f7fba27d64f3
Jason Jackson <jasonc.jackson@gmail.com>
2024-11-12 15:52:02 -0500

    remove versions as no local resources specified

--- versions.tf
+++ versions.tf
@@ -4,11 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
     }
   }
 }
